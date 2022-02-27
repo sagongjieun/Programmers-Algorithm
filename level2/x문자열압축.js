4bc5b904ddaab3b5788dfb55f2fb3c2532da9c4c
sagongjieun <gonii0821@naver.com>
2022-02-27 21:54:32 +0900

    220227

--- level2/x문자열압축.js
+++ level2/x문자열압축.js
@@ -1,9 +1,24 @@
 function solution(s) {
-    var answer = 0;
-    return answer;
+  let answer = [];
+
+  if (s.length === 1) return 1;
+
+  for (let i = 1; i <= parseInt(s.length / 2); i++) {
+    for (let j = 0; j < s.length; j += i) {
+      let current = s.substr(j, i);
+    }
+  }
 }
 
 const s = "aabbaccc"; //7
 const result = solution(s);
 console.log(result);
 
+// 안 잘려지는 경우는 s.length 반환
+
+/*
+const a = "AAAbbCC";
+
+a.match(/[A-z]{3}/);
+// AAA
+*/
